eda1413623d317400bd0e9713227feef645061a7
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-03-04 20:25:57 +0800

    ✨ feat: About Us

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,11 @@
 import catImage from './images/cat.jpg';
 import playerFormBg from './images/player-form-bg.jpg';
+import carouselImage1 from './images/carousel-img-1.png';
+import carouselImage2 from './images/carousel-img-2.png';
+import carouselImage3 from './images/carousel-img-3.png';
+import carouselImage4 from './images/carousel-img-4.png';
+import carouselImage5 from './images/carousel-img-5.png';
+import carouselImage6 from './images/carousel-img-6.png';
 
 import basketballIcon from './icons/Basketball.svg';
 import burgerIcon from './icons/Burger.svg';
@@ -10,6 +16,12 @@ import basketballVideo from './videos/Basketball.mp4';
 export const Images = {
   catImage,
   playerFormBg,
+  carouselImage1,
+  carouselImage2,
+  carouselImage3,
+  carouselImage4,
+  carouselImage5,
+  carouselImage6,
 };
 
 export const Icons = {
--- src/modules/AboutUs/containers/AboutUs.js
+++ src/modules/AboutUs/containers/AboutUs.js
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { onDecrement, onIncrement } from '../../../services/action';
+import Carousel from 'react-material-ui-carousel';
+import { Images } from '../../../assets';
+import { Box, Card } from '@mui/material';
 
 export default class AboutUs extends Component {
   render() {
-    return <section>This is about us page!</section>;
+    return (
+      <Box component="section" sx={{ width: '100%', height: '100%' }}>
+        <Box
+          sx={{
+            width: '500px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+            backgroundColor: 'transparent',
+          }}
+        >
+          <Carousel sx={{ width: '100%' }} animation="slide" height="300px">
+            <Box sx={{ width: '100%', height: '100%' }}>
+              <img
+                src={Images.carouselImage1}
+                alt="Carousel Image 1"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </Box>
+            <Box sx={{ width: '100%', height: '100%' }}>
+              <iframe
+                style={{ width: '100%', height: '100%', display: 'block' }}
+                src="https://www.youtube.com/embed/ob702b9PXvw"
+                title="LeBron James Drops SEASON-HIGH 47 PTS On His Birthday | December 30, 2022"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              />
+            </Box>
+            <Box sx={{ width: '100%', height: '100%' }}>
+              <img
+                src={Images.carouselImage3}
+                alt="Carousel Image 3"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </Box>
+            <Box sx={{ width: '100%', height: '100%' }}>
+              <img
+                src={Images.carouselImage4}
+                alt="Carousel Image 4"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </Box>
+            <Box sx={{ width: '100%', height: '100%' }}>
+              <img
+                src={Images.carouselImage5}
+                alt="Carousel Image 5"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </Box>
+            <Box sx={{ width: '100%', height: '100%' }}>
+              <img
+                src={Images.carouselImage6}
+                alt="Carousel Image 6"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </Box>
+          </Carousel>
+        </Box>
+      </Box>
+    );
   }
 }
