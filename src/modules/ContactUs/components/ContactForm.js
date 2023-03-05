b4ac73e19c221e09bf877be9666c41296c183e53
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-03-05 19:26:09 +0800

    🚑 fix: Contact Us Validation

--- src/modules/ContactUs/components/ContactForm.js
+++ src/modules/ContactUs/components/ContactForm.js
@@ -1,7 +1,168 @@
-import { Card, TextField } from '@mui/material';
-import React from 'react';
+import { Button, Card, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { onModalOpen } from '../../../services/action';
 
 export const ContactForm = () => {
+  const [contactForm, setContactForm] = useState({
+    nameInputValue: '',
+    emailInputValue: '',
+    contactInputValue: '',
+    messageInputValue: '',
+    error: {
+      nameInputError: '',
+      emailInputError: '',
+      contactInputError: '',
+      messageInputError: '',
+    },
+    isNameValid: false,
+    isEmailValid: false,
+    isContactValid: false,
+    isMessageValid: false,
+  });
+  const dispatch = useDispatch();
+
+  const validate = (type, value) => {
+    if (type === 'nameInput') {
+      let isNameValid = false;
+      let nameInputError = '';
+      if (value === '') {
+        nameInputError = 'Name cannot be empty.';
+        isNameValid = false;
+      } else {
+        nameInputError = '';
+        isNameValid = true;
+      }
+      setContactForm((prev) => {
+        return {
+          ...prev,
+          error: { ...prev.error, nameInputError },
+          isNameValid,
+        };
+      });
+      return isNameValid;
+    } else if (type === 'emailInput') {
+      let isEmailValid = false;
+      let emailInputError = '';
+      if (value === '') {
+        emailInputError = 'Email cannot be empty.';
+        isEmailValid = false;
+      } else {
+        emailInputError = '';
+        isEmailValid = true;
+      }
+      setContactForm((prev) => {
+        return {
+          ...prev,
+          error: { ...prev.error, emailInputError },
+          isEmailValid,
+        };
+      });
+      return isEmailValid;
+    } else if (type === 'contactInput') {
+      let isContactValid = false;
+      let contactInputError = '';
+      if (value === '') {
+        contactInputError = 'Contact cannot be empty.';
+        isContactValid = false;
+      } else {
+        contactInputError = '';
+        isContactValid = true;
+      }
+      setContactForm((prev) => {
+        return {
+          ...prev,
+          error: { ...prev.error, contactInputError },
+          isContactValid,
+        };
+      });
+      return isContactValid;
+    } else if (type === 'messageInput') {
+      let isMessageValid = false;
+      let messageInputError = '';
+      if (value === '') {
+        messageInputError = 'Message cannot be empty.';
+        isMessageValid = false;
+      } else {
+        messageInputError = '';
+        isMessageValid = true;
+      }
+      setContactForm((prev) => {
+        return {
+          ...prev,
+          error: { ...prev.error, messageInputError },
+          isMessageValid,
+        };
+      });
+      return isMessageValid;
+    }
+  };
+
+  const onChangHandler = (type, value) => {
+    validate(type, value);
+    if (type === 'nameInput') {
+      setContactForm((prev) => {
+        return { ...prev, nameInputValue: value };
+      });
+    } else if (type === 'emailInput') {
+      setContactForm((prev) => {
+        return { ...prev, emailInputValue: value };
+      });
+    } else if (type === 'contactInput') {
+      setContactForm((prev) => {
+        return { ...prev, contactInputValue: value };
+      });
+    } else if (type === 'messageInput') {
+      setContactForm((prev) => {
+        return { ...prev, messageInputValue: value };
+      });
+    }
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (
+      validate('nameInput', contactForm.nameInputValue) &&
+      validate('emailInput', contactForm.emailInputValue) &&
+      validate('contactInput', contactForm.contactInputValue) &&
+      validate('messageInput', contactForm.messageInputValue)
+    ) {
+      setContactForm((prev) => {
+        return {
+          nameInputValue: '',
+          emailInputValue: '',
+          contactInputValue: '',
+          messageInputValue: '',
+          error: {
+            nameInputError: '',
+            emailInputError: '',
+            contactInputError: '',
+            messageInputError: '',
+          },
+          isNameValid: false,
+          isEmailValid: false,
+          isContactValid: false,
+          isMessageValid: false,
+        };
+      });
+      dispatch(
+        onModalOpen({
+          visible: true,
+          title: 'Success',
+          description: 'Contact form submitted.',
+        })
+      );
+    } else {
+      dispatch(
+        onModalOpen({
+          visible: true,
+          title: 'Failed',
+          description: 'Player and score were not added\nPlease try again.',
+        })
+      );
+    }
+  };
+
   const textFieldStyle = {
     ['& label.Mui-focused']: {
       color: 'white',
@@ -23,6 +184,8 @@ export const ContactForm = () => {
     },
   };
 
+  const errorTextStyle = { mt: 1, color: 'yellow', fontSize: 12 };
+
   return (
     <Card
       sx={{
@@ -41,15 +204,71 @@ export const ContactForm = () => {
           display: 'flex',
           flexDirection: 'column',
         }}
+        onSubmit={formSubmitHandler}
       >
-        <TextField variant="standard" label="Name" sx={textFieldStyle} />
-        <TextField variant="standard" label="Email" sx={textFieldStyle} />
+        <TextField
+          variant="standard"
+          label="Name"
+          sx={textFieldStyle}
+          value={contactForm.nameInputValue}
+          onChange={(e) => {
+            onChangHandler('nameInput', e.target.value);
+          }}
+        />
+        <Typography sx={errorTextStyle}>
+          {contactForm.error.nameInputError === ''
+            ? '\u00A0'
+            : contactForm.error.nameInputError}
+        </Typography>
+        <TextField
+          variant="standard"
+          label="Email"
+          sx={textFieldStyle}
+          value={contactForm.emailInputValue}
+          onChange={(e) => {
+            onChangHandler('emailInput', e.target.value);
+          }}
+        />
+        <Typography sx={errorTextStyle}>
+          {contactForm.error.emailInputError === ''
+            ? '\u00A0'
+            : contactForm.error.emailInputError}
+        </Typography>
         <TextField
           variant="standard"
           label="Contact Number"
           sx={textFieldStyle}
+          value={contactForm.contactInputValue}
+          onChange={(e) => {
+            onChangHandler('contactInput', e.target.value);
+          }}
         />
-        <TextField variant="standard" label="Message" sx={textFieldStyle} />
+        <Typography sx={errorTextStyle}>
+          {contactForm.error.contactInputError === ''
+            ? '\u00A0'
+            : contactForm.error.contactInputError}
+        </Typography>
+        <TextField
+          variant="standard"
+          label="Message"
+          sx={textFieldStyle}
+          value={contactForm.messageInputValue}
+          onChange={(e) => {
+            onChangHandler('messageInput', e.target.value);
+          }}
+        />{' '}
+        <Typography sx={errorTextStyle}>
+          {contactForm.error.messageInputError === ''
+            ? '\u00A0'
+            : contactForm.error.messageInputError}
+        </Typography>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ backgroundColor: '#539165', mt: 8 }}
+        >
+          Submit
+        </Button>
       </form>
     </Card>
   );
